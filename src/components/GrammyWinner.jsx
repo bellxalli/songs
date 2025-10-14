5e3ebad7e81fd36fbfd71f21f230e97ac6b6dfe8
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-10-14 13:52:50 -0700

    changes to GrammyWinner/Songs

---
+++ src/components/GrammyWinner.jsx
@@ -0,0 +1,12 @@
+export default function GrammyWinner(props){
+
+        if(props.isGrammyWinner){
+
+            return <p> This album is a Grammy winner! </p>
+        } else {
+
+            return <p> This album is not a Grammy winner, but it's won people's hearts! </p>
+
+        }
+
+}
--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,7 +1,7 @@
 export default function Song(props){
 
     return(
-        <div>
+        <div className="album-card">
             <h3> {props.title} </h3>
             <h4> By {props.artist} </h4>
             <p> The album was released in {props.year}. </p>
@@ -9,4 +9,12 @@ export default function Song(props){
             <ul> {props.genres.map((s, index) => <li key={index}>{s}</li>)} </ul>
         </div>
     )
+}
+
+export function Link(){
+
+
+
+
+    
 }
