a373092abd1d202153fdf9a399bf0573cff4225e
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-10-14 14:29:54 -0700

    add clickable link to midnights album

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -2,10 +2,13 @@ import Ratings from "./Ratings"
 import GrammyStatus from "./GrammyWinner"
 
 export default function Song(props){
-console.log("isGrammyWinner prop:", props.isGrammyWinner);
+
+    const handleClick = () => {
+        if (props.link) window.open(props.link, "_blank");
+    }
 
     return(
-        <div className="album-card">
+        <div className="album-card" onClick={handleClick} style={{ cursor: props.link ? "pointer" : "default" }}>
             <h3> {props.title} </h3>
             <h4> By {props.artist} </h4>
             <p> The album was released in {props.year}. </p>
@@ -14,15 +17,4 @@ console.log("isGrammyWinner prop:", props.isGrammyWinner);
             <ul> {props.genres.map((s, index) => <li key={index}>{s}</li>)} </ul>
         </div>
     )
-}
-
-export function Link(){
-
-    if(props.link){
-
-            return <p> This album is a Grammy winner! </p>
-        }
-
-
-
 }
