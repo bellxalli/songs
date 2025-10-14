0beb52b0de1c75e1ffaeb34d119a804cefaa4be7
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-10-14 13:40:55 -0700

    added styles.css + GrammyWinner.jsx + Ratings.jsx

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -4,9 +4,11 @@ export default function Song(props){
 
         <div>
 
-        <h1> Songs! </h1>
+            <h3> {props.title} </h3>
+            <h4> By {props.artist} </h4>
+            <p> The album was released in {props.year}. </p>
 
-        
+            <ul> {props.genres.map((s, index) => <li key={index}>{s}</li>)} </ul>
 
 
         </div>
