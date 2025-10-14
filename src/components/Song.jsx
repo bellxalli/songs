d734c4bd8fd0f83f889ffb36a67720a2a963feda
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-14 14:22:18 -0700

    fixed rendering issue for grammy winner component

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -2,13 +2,14 @@ import Ratings from "./Ratings"
 import GrammyStatus from "./GrammyWinner"
 
 export default function Song(props){
+console.log("isGrammyWinner prop:", props.isGrammyWinner);
 
     return(
         <div className="album-card">
             <h3> {props.title} </h3>
             <h4> By {props.artist} </h4>
             <p> The album was released in {props.year}. </p>
-            <GrammyStatus />
+            <GrammyStatus isGrammyWinner={props.isGrammyWinner}/>
             <Ratings rating={props.rating} />
             <ul> {props.genres.map((s, index) => <li key={index}>{s}</li>)} </ul>
         </div>
