563ee19fb401f52ea734ad24984296926124059d
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-14 13:57:04 -0700

    imported grammy for songs

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,4 +1,5 @@
 import Ratings from "./Ratings"
+import GrammyStatus from "./GrammyWinner"
 
 export default function Song(props){
 
@@ -7,6 +8,7 @@ export default function Song(props){
             <h3> {props.title} </h3>
             <h4> By {props.artist} </h4>
             <p> The album was released in {props.year}. </p>
+            <GrammyStatus />
             <Ratings />
             <ul> {props.genres.map((s, index) => <li key={index}>{s}</li>)} </ul>
         </div>
